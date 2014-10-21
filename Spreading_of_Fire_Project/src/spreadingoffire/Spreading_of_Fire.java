b389b5a87c3d07d5a5b7af433c1a772a0dfb5045
Songpol <56130500212Songpol@users.noreply.github.com>
2014-10-21 18:19:21 +0700

    update spreading_of_fire.java

--- Spreading_of_Fire_Project/src/spreadingoffire/Spreading_of_Fire.java
+++ Spreading_of_Fire_Project/src/spreadingoffire/Spreading_of_Fire.java
@@ -1,1 +1,33 @@
+import java.awt.Container;
+import javax.swing.JFrame;
+
+/**
+ * The visualization class of project
+ *
+ * @author SpreadingOfFire
+ * @version 21/10/2014
+ */
+public class Spreading_of_Fire extends JFrame {
+  
+  private Model model;
+  private View view;
+  
+  /**
+   * Constructor - create the frame
+   */
+  public Spreading_of_Fire() {
+    super("Spreading of Fire"); //change the name to "Spreading of Fire"
+    Container contentPane = this.getContentPane();
+    view = new View();
+    view.setSeeValue(true); //user can see the value of each cell. If do not want to see, set to false
+    view.setSeeEdge(true); //user can see the edge of each cell. If do not want to see, set to false
+    model = new Model(view,25);
+    model.initForest();
+    setResizable(false); // cannot change the size of frame
+    setSize(660, 683); 
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    contentPane.add(view);
+    setVisible(true);
+  }
+}
 
