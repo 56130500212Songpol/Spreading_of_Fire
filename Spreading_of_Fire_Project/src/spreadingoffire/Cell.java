0435dbce05821f572f2857823a3d32a563df2d45
Songpol <56130500212@st.sit.kmutt.ac.th>
2014-10-21 18:10:21 +0700

    add color and value, method that change view

---
+++ Spreading_of_Fire_Project/src/spreadingoffire/Cell.java
@@ -0,0 +1,46 @@
+/**
+ * This cell class that contains the state of cell
+ *
+ * @author SpreadingOfFire
+ * @version 21/10/2014
+ */
+public class Cell {
+  
+  public static final int EMPTY = 0, TREE = 1, BURNING = 2;
+  private int state;
+  
+  /**
+   * Constructor - create the cell by EMPTY state
+   */
+  public Cell() {
+    state = Cell.EMPTY;
+  }
+  
+  /**
+   * Constructor - create the cell by state
+   *
+   * @param state
+   */
+  public Cell(int state) {
+    this.state = state;
+  }
+  
+  /**
+   * get the state of cell
+   *
+   * @return state
+   */
+  public int getState() {
+    return state;
+  }
+  
+  /**
+   * set the state of cell
+   *
+   * @param state
+   */
+  public void setState(int state) {
+    this.state = state;
+  }
+  
+}
