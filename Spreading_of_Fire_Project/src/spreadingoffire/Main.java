7b741cab93ac42f472c5ca6fd79fb5ebde193b0b
Songpol <56130500212Songpol@users.noreply.github.com>
2014-10-21 18:17:26 +0700

    Update Main.java

--- Spreading_of_Fire_Project/src/spreadingoffire/Main.java
+++ Spreading_of_Fire_Project/src/spreadingoffire/Main.java
@@ -1,11 +1,1 @@
-/**
- * The main class of project
- *
- * @author SpreadingOfFire
- * @version 21/10/2014
- */
-public class Main{
-  public static void main(String args[]){
-    new Spreading_of_Fire();
-  }
-}
+
