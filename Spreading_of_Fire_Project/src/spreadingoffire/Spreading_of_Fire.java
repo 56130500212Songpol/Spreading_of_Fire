95f7f5769d518fdd152a694e1c596ca3a940efc2
Songpol <56130500212Songpol@users.noreply.github.com>
2014-10-27 15:46:15 +0700

    Update Spreading_of_Fire.java

--- Spreading_of_Fire_Project/src/spreadingoffire/Spreading_of_Fire.java
+++ Spreading_of_Fire_Project/src/spreadingoffire/Spreading_of_Fire.java
@@ -1,1 +1,24 @@
+import java.awt.Container;
+import javax.swing.JFrame;
 
+/**
+ * The visualization class of project(Frame class)
+ *
+ * @author OOSD Project Group 5
+ * @version 26/10/2014
+ */
+public class Spreading_of_Fire extends JFrame {
+
+    private Controller controller;
+
+    public Spreading_of_Fire() {
+        super("Spreading of Fire");
+        Container contentPane = this.getContentPane();
+        controller = new Controller();
+        setResizable(false);
+        setSize(900, 683);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        contentPane.add(controller);
+        setVisible(true);
+    }
+}
