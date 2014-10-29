c772b7d3f7b02a6cd6ee29d1137ecbd102f0aad2
Songpol <56130500212@st.sit.kmutt.ac.th>
2014-10-29 18:04:13 +0700

    Revert "Revert "add resize forest""
    This reverts commit fcba739ffd68d80f57bf642f7d90905b9a14d2ef.

--- Spreading_of_Fire_Project/src/spreadingoffire/Controller.java
+++ Spreading_of_Fire_Project/src/spreadingoffire/Controller.java
@@ -5,22 +5,31 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.JButton;
 import javax.swing.JSlider;
+import javax.swing.JRadioButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.ButtonGroup;
 
 /**
  * The controller class of project which contain buttons and sliders
  *
  * @author OOSD Project Group 5
- * @version 28/10/2014
+ * @version 29/10/2014
  */
 public class Controller extends JPanel {
   
-  private JButton reset, auto, step;
+  private JButton reset, auto, step, seeValue, seeBorder;
   private JSlider probCatchValue, probTreeValue, probBurningValue, delayValue;
-  private JLabel how,probCatch, probTree, probBurning, delay, probCatchShow, probTreeShow, probBurningShow, delayShow;
+  private JRadioButton tiny, small, medium, large, huge, world;
+  private JRadioButton middle, random;
+  private ButtonGroup group, group2;
+  private JLabel size, probCatch, probTree, probBurning, delay, position,
+    how, probCatchShow, probTreeShow, probBurningShow, delayShow;
+  private JPanel buttonArea;
   private View view;
   private Model model;
+  private boolean isRandom = false, isMiddle = false, isChangeSetting = false,
+    isOnWorld = false;
   
   /**
    * Constructor - create controller panel 
@@ -31,6 +40,8 @@ public class Controller extends JPanel {
     model.initForest();
     setLayout(new GridLayout());
     addInteractButton(); // add reset button, and auto button,step button to controller panel
+    addForestSizeRadioButton(); // add resize forest button, tiny, small, medium, large, huge, and world
+    addInitialBurningTreeRadioButton(); // add initial burning tree button, random or middle
     addProbSlider(); // add probCatch slider, probTree slider, probBuring slider, and delay slider to controller panel
     add(view); // add view to controller panel
   }
@@ -45,6 +56,8 @@ public class Controller extends JPanel {
     reset = new JButton("Regrow Trees");
     auto = new JButton("Auto Spread");
     step = new JButton("Step-by-Step");
+    seeValue = new JButton("See Value of Cell");
+    seeBorder = new JButton("See Border of Cell");
     
     step.addActionListener(new ActionListener() {
       @Override
@@ -78,10 +91,221 @@ public class Controller extends JPanel {
       }
     });
     
+    seeValue.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (!isOnWorld) {
+          if (view.isSeeValue() == false) {
+            view.setSeeValue(true);
+          } else if (view.isSeeValue() == true) {
+            view.setSeeValue(false);
+          }
+          if (!isChangeSetting) {
+            view.repaint();
+          }
+        }
+      }
+    });
+    
+    seeBorder.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (!isOnWorld) {
+          if (view.isSeeBorder() == false) {
+            view.setSeeBorder(true);
+          } else if (view.isSeeBorder() == true) {
+            view.setSeeBorder(false);
+          }
+          if (!isChangeSetting) {
+            view.repaint();
+          }
+        }
+      }
+    });
+    
     view.add(reset);
     view.add(auto);
     view.add(step);
+    view.add(seeValue);
+    view.add(seeBorder);
   }
+  
+  /**
+   * Add text and radiobutton to resize the field
+   *
+   */
+  private void addForestSizeRadioButton() {
+        size = new JLabel("Choose Forest size :");
+        view.add(size);
+        group = new ButtonGroup();
+
+        tiny = new JRadioButton("Tiny");
+        tiny.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                view.setPixel(24);
+                model.setNumCell(25);
+                if (isRandom) {
+                    model.setRandomPositionX();
+                    model.setRandomPositionY();
+                } else {
+                    model.setPositionXonMiddle();
+                    model.setPositionYonMiddle();
+                }
+                isChangeSetting = true;
+                isOnWorld = false;
+            }
+        });
+
+        small = new JRadioButton("Small");
+        small.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                view.setPixel(15);
+                model.setNumCell(41);
+                if (isRandom) {
+                    model.setRandomPositionX();
+                    model.setRandomPositionY();
+                } else {
+                    model.setPositionXonMiddle();
+                    model.setPositionYonMiddle();
+                }
+                isChangeSetting = true;
+                isOnWorld = false;
+            }
+        });
+
+        medium = new JRadioButton("Medium");
+        medium.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                view.setPixel(10);
+                model.setNumCell(63);
+                if (isRandom) {
+                    model.setRandomPositionX();
+                    model.setRandomPositionY();
+                } else {
+                    model.setPositionXonMiddle();
+                    model.setPositionYonMiddle();
+                }
+                isChangeSetting = true;
+                isOnWorld = false;
+            }
+        });
+
+        large = new JRadioButton("Large");
+        large.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                view.setPixel(8);
+                model.setNumCell(79);
+                if (isRandom) {
+                    model.setRandomPositionX();
+                    model.setRandomPositionY();
+                } else {
+                    model.setPositionXonMiddle();
+                    model.setPositionYonMiddle();
+                }
+                isChangeSetting = true;
+                isOnWorld = false;
+            }
+        });
+
+        huge = new JRadioButton("Huge");
+        huge.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                view.setPixel(6);
+                model.setNumCell(107);
+                if (isRandom) {
+                    model.setRandomPositionX();
+                    model.setRandomPositionY();
+                } else {
+                    model.setPositionXonMiddle();
+                    model.setPositionYonMiddle();
+                }
+                isChangeSetting = true;
+                isOnWorld = false;
+            }
+        });
+
+        world = new JRadioButton("World");
+        world.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                view.setPixel(1);
+                model.setNumCell(650);
+                if (isRandom) {
+                    model.setRandomPositionX();
+                    model.setRandomPositionY();
+                } else {
+                    model.setPositionXonMiddle();
+                    model.setPositionYonMiddle();
+                }
+                view.setSeeBorder(false);
+                view.setSeeValue(false);
+                isChangeSetting = true;
+                isOnWorld = true;
+            }
+        });
+
+        group.add(tiny);
+        group.add(small);
+        group.add(medium);
+        group.add(large);
+        group.add(huge);
+        group.add(world);
+        view.add(tiny);
+        view.add(small);
+        view.add(medium);
+        view.add(large);
+        view.add(huge);
+        view.add(world);
+    }
+
+  /**
+   * Add text and radiobutton to set the initial burning tree
+   *
+   */
+    private void addInitialBurningTreeRadioButton() {
+        position = new JLabel("Select position of initial burn tree :");
+        view.add(position);
+        buttonArea = new JPanel();
+        buttonArea.setLayout(new GridLayout(1, 2));
+        group2 = new ButtonGroup();
+
+        middle = new JRadioButton("Middle Tree");
+        random = new JRadioButton("Random tree");
+
+        middle.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isRandom = false;
+                isMiddle = true;
+                model.setPositionXonMiddle();
+                model.setPositionYonMiddle();
+                System.out.println(model.getPositionX());
+                System.out.println(model.getPositionY());
+            }
+        });
+        random.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isRandom = true;
+                isMiddle = false;
+                model.setRandomPositionX();
+                model.setRandomPositionY();
+                System.out.println(model.getPositionX());
+                System.out.println(model.getPositionY());
+            }
+        });
+
+        group2.add(middle);
+        group2.add(random);
+        buttonArea.add(middle);
+        buttonArea.add(random);
+        view.add(buttonArea);
+    }
   
   
   /**
@@ -162,5 +386,3 @@ public class Controller extends JPanel {
     view.add(delayValue);
   }
 }
-
-
