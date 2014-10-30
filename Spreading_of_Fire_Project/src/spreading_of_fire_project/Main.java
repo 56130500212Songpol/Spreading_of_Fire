1cb490f28ebf1f646334cc3c29e28892b3df524c
Songpol <56130500212@st.sit.kmutt.ac.th>
2014-10-30 22:46:52 +0700

    update all code

---
+++ Spreading_of_Fire_Project/src/spreading_of_fire_project/Main.java
@@ -0,0 +1,14 @@
+package spreading_of_fire_project;
+
+/**
+ * The main class of project use for run the projects
+ *
+ * @author OOSD Project Group 5
+ * @version 30/10/2014
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        new Spreading_of_Fire();
+    }
+}
